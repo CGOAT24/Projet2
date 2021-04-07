c10fdfbd56cffacadaeab25d50b91ba3f3b9e870
Chad Gauthier <chad.gauthier32@gmail.com>
2021-04-07 14:35:12 -0400

    controles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,12 @@ public class Player : MonoBehaviour{
     [SerializeField] private GameObject _missilePrefab = default;
 
     private Vector3 _direction;
-    private Rigidbody _rb;
+    private float _rotation;
+    private Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start(){
-        _rb = this.GetComponent<Rigidbody>();
+        _rb = this.GetComponent<Rigidbody2D>();
 
         Set_Controls();
     }
@@ -28,21 +29,40 @@ public class Player : MonoBehaviour{
         InputAction fireAction = _actionAsset.FindAction("Fire");
         fireAction.performed += fireAction_performed;
         fireAction.Enable();
+
+        InputAction turnAction = _actionAsset.FindAction("Turn");
+        turnAction.performed += turnAction_performed;
+        turnAction.canceled += turnAction_canceled;
+        turnAction.Enable();
     }
 
     private void moveAction_performed(InputAction.CallbackContext obj){
         Vector2 direction2D = obj.ReadValue<Vector2>();
-        _direction = new Vector3(direction2D.x, direction2D.y, 0);
+        _direction = new Vector3(0, direction2D.y, 0);
         _direction.Normalize();
         _rb.velocity = _direction * _vitesse;
+
+        _rotation = direction2D.x;
+        _rb.AddTorque(_rotation * _vitesse);
     }
 
     private void moveAction_canceled(InputAction.CallbackContext obj){
         _direction = new Vector3(0f, 0f, 0f);
         _rb.velocity = _direction * _vitesse;
+
+        _rotation = 0f;
     }
 
     private void fireAction_performed(InputAction.CallbackContext obj){
         Instantiate(_missilePrefab, transform.position, Quaternion.identity);
+    }
+
+    private void turnAction_performed(InputAction.CallbackContext obj){
+
+    }
+
+    private void turnAction_canceled(InputAction.CallbackContext obj){
+
+        
     }
 }
