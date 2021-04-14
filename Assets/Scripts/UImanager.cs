55ba49f057f135acfceb05e670f2b197d7cd45f5
Chad Gauthier <chad.gauthier32@gmail.com>
2021-04-14 14:26:12 -0400

    TMPro et pause

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -2,25 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class UImanager : MonoBehaviour{
     private bool _isPaused = false; 
 
     [SerializeField] private InputActionAsset _actionAsset = default;
+    [SerializeField] private GameObject _pausePanel = default;
+    [SerializeField] private GameObject _pauseText = default;
 
     void Start(){
         InputAction pauseMenu = _actionAsset.FindAction("PauseMenu");
         pauseMenu.performed += pauseMenu_performed;
+
         pauseMenu.Enable();
+        _pausePanel.SetActive(false);
     }
 
     void pauseMenu_performed(InputAction.CallbackContext obj){
         if(!_isPaused){
             _isPaused = true;
             Time.timeScale = 0;
+            _pausePanel.SetActive(true);
         }
-        else{
+        else if(_isPaused){
             _isPaused = false;
+            _pausePanel.SetActive(false);
             Time.timeScale = 1;
         }
     }
