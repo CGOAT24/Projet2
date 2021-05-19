617f7b46dd105e2eaf1711396f62c90e596a2107
unknown <chad.gauthier32@gmail.com>
2021-05-19 17:52:02 -0400

    ddd

---
+++ Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Transform _player;
+    private Vector2 _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _target = new Vector2(_player.position.x, _player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position.x == _target.x && transform.position.y == _target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
